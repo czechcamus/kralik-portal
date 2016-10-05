81c76983e784386819bdcc500d658209d4ad19de
czechcamus <czechcamus@gmail.com>
2016-10-05 07:24:57 +0200

    kancl 5.10.2016, 7:24

--- private/views/site/index.php
+++ private/views/site/index.php
@@ -12,15 +12,15 @@ $this->title = 'DENNY+ Milan Králík';
 				<div class="caption center-align">
 					<h3>Čištění autointeriérů</h3>
 					<h5 class="light grey-text text-lighten-3">Profesionální práce je naší vizitkou.</h5>
-					<a class="waves-effect waves-light btn-large"><i class="material-icons right">send</i>zjistit více</a>
+					<a href="http://denny-plus.blog.cz/" class="waves-effect waves-light btn-large"><i class="material-icons right">send</i>zjistit více</a>
 				</div>
 			</li>
 			<li>
 				<img src="<?= Yii::$app->request->baseUrl; ?>/img/pokoj.jpg">
 				<div class="caption center-align">
 					<h3>Ubytování v Dačicích</h3>
-					<h5 class="light grey-text text-lighten-3">Ubytování v klidném prostředí.</h5>
-					<a class="waves-effect waves-light btn-large"><i class="material-icons right">send</i>zjistit více</a>
+					<h5 class="light grey-text text-lighten-3">Pobyt v klidném prostředí.</h5>
+					<a href="http://www.ubytovani-dacice.cz" class="waves-effect waves-light btn-large"><i class="material-icons right">send</i>zjistit více</a>
 				</div>
 			</li>
 		</ul>
